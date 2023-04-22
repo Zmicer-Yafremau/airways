ace4ea17a2594aab6f1c46ddf5ae9c01149ee27c
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2023-04-22 12:42:22 +0300

    feat: add auth structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import AppRoutingModule from "./app-routing.module";
+import { RegisterComponent } from "./modules/auth/components/register/register/register.component";
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
