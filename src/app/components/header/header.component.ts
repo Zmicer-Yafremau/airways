058d0b18aaee4b236728077ffbab94ac2ca9bb33
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2023-04-28 23:06:13 +0300

    feat: add name, mail and phone validation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
-import { LoginComponent } from 'src/app/modules/auth/components/login/login.component';
-import { MatDialog } from '@angular/material/dialog';
+import { Component } from "@angular/core";
+import { LoginComponent } from "src/app/modules/auth/components/login/login.component";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-
-  public constructor (private matDialog: MatDialog){}
-  public launhAuthDialog(){
+  public constructor(private matDialog: MatDialog) {}
+  public launhAuthDialog() {
     this.matDialog.open(LoginComponent);
   }
 }
--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,24 +1,52 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { ThemePalette } from '@angular/material/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators, ValidationErrors } from "@angular/forms";
+import { ThemePalette } from "@angular/material/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public reg_form!: FormGroup;
+  public hide = true;
+  public agree = false;
+  //ublic email = new FormControl("", [Validators.required, Validators.email]);
+  public f_Name = new FormControl("", [Validators.required]);
+  public codeControl = new FormControl("93" as ThemePalette);
 
-public hide = true;
-public agree = false;
-public email = new FormControl('', [Validators.required, Validators.email]);
-public codeControl = new FormControl('93' as ThemePalette);
+  public constructor(public fb: FormBuilder) {}
 
-getErrorMessage() {
-  if (this.email.hasError('required')) {
-    return 'You must enter a value';
+  public ngOnInit(): void {
+    this.reg_form = this.fb.group({
+      r_email: ["", [Validators.required, Validators.email]],
+      f_name: ["", [Validators.required, this.nameValidator]],
+      l_name: ["", [Validators.required, this.nameValidator]],
+      phone: ["", [Validators.required, this.phoneValidator]],
+    });
   }
 
-  return this.email.hasError('email') ? 'Not a valid email' : '';
-}
+  public getMailErrorMessage() {
+    if (this.reg_form.controls["r_email"].hasError("required")) {
+      return "You must enter a value";
+    }
+
+    return this.reg_form.controls["r_email"].hasError("email") ? "Not a valid email" : "";
+  }
+  public nameValidator(control: FormControl): ValidationErrors | null {
+    if (control.value) {
+      if (/\d/.test(control.value)) {
+        return { name: true };
+      }
+    }
+    return null;
+  }
+  public phoneValidator(control: FormControl): ValidationErrors | null {
+    if (control.value) {
+      if (!/\+[0-9]{9,}/.test(control.value)) {
+        return { phone: true };
+      }
+    }
+    return null;
+  }
 }
